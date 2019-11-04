9c5994a9d209866438a435f636ff64890a955c36
jihyun-um <ashleyum20@gmail.com>
2019-11-04 07:07:42 +0000

    Add missing annotations

--- src/main/java/com/jihyunum/spring/data/jpa/example/repository/AuthorRepository.java
+++ src/main/java/com/jihyunum/spring/data/jpa/example/repository/AuthorRepository.java
@@ -2,9 +2,11 @@ package com.jihyunum.spring.data.jpa.example.repository;
 
 import com.jihyunum.spring.data.jpa.example.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
     Optional<Author> findByName(String name);
 }
--- src/main/java/com/jihyunum/spring/data/jpa/example/repository/BookRepository.java
+++ src/main/java/com/jihyunum/spring/data/jpa/example/repository/BookRepository.java
@@ -2,9 +2,11 @@ package com.jihyunum.spring.data.jpa.example.repository;
 
 import com.jihyunum.spring.data.jpa.example.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findByTitle(String title);
 }
