c7f8e55a387edaafda1ef757630ee330cd48822a
jihyun-um <ashleyum20@gmail.com>
2019-11-04 06:15:04 +0000

    Initial commit

---
+++ src/test/java/com/jihyunum/spring/data/jpa/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jihyunum.spring.data.jpa.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
